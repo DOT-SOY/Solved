08f8fc94a9f0b12ca336d1eb10fa8e1a6863c24b
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-10-30 13:46:43 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.07 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int[] numbers) {
-        for(int i = 0; i < numbers.length; i++) {
-        	numbers[i] = numbers[i]*2;
-        	System.out.println(i+"번째 "+numbers[i]);
+        int[] answer = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
         }
-        int[] answer = {};
-        answer = numbers;
+
         return answer;
     }
 }
