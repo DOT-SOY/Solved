4a169c7155dca8659de3bbfdaf1a6b89c8d8036f
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-10-30 10:46:04 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+    	StringBuilder sb = new StringBuilder(my_string);
+    	sb.replace(s, overwrite_string.length() + s, overwrite_string);
+        return sb.toString();
+    }
+}
