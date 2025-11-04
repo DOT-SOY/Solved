84690f37d9a316ad9d12e59f7bbb1169e71977b3
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-11-04 13:38:51 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
         int num = sc.nextInt();
-        if(num > 89) {
-        	System.out.println("A");
-        }else if(num > 79 && num < 90) {
-        	System.out.println("B");
-        }else if(num > 69 && num < 80) {
-        	System.out.println("C");
-        }else if(num > 59 && num < 70) {
-        	System.out.println("D");
-        }else {
-        	System.out.println("F");
+
+        if (num >= 90) {
+            System.out.println("A");
+        } else if (num >= 80) {
+            System.out.println("B");
+        } else if (num >= 70) {
+            System.out.println("C");
+        } else if (num >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
     }
 }
