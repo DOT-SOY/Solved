b7ce4468e4a843aa4506a3b9d08a0f9009669bd7
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-11-04 16:07:45 +0900

    [Bronze II] Title: 나머지, Time: 176 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> hash = new HashSet<>();
+        
+        for(int i = 0; i < 10; i++) {
+        	int num = sc.nextInt();
+        	hash.add(num % 42);
+        }
+        
+        System.out.println(hash.size());
+        
+    }
+}
