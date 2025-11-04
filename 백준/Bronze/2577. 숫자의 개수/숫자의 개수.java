1ff029f9e8ba68c84cad158d7d0ac5aea9121c3a
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-11-04 15:19:15 +0900

    [Bronze II] Title: 숫자의 개수, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        
-        String str = (num1 * num2 * num3) + "";
-        String[] strArr = str.split("");
-        int[] intArr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        
-        for(int i = 0; i < strArr.length; i++) {
-        	intArr[Integer.parseInt(strArr[i])]++;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int num = a * b * c;
+        int[] count = new int[10];
+
+        while (num > 0) {
+            count[num % 10]++;  // 마지막 자리 숫자 카운트
+            num /= 10;          // 마지막 자리 제거
         }
-        
-        for (int n : intArr) {
-            System.out.print(n + "\n");
+
+        for (int n : count) {
+            System.out.println(n);
         }
     }
 }
