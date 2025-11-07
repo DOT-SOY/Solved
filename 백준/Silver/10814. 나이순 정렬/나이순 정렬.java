7d06742c02315efa09e975007eecadab9144f6e3
woometan <131198770+DOT-SOY@users.noreply.github.com>
2025-11-08 02:10:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 552 ms, Memory: 45800 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,29 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	int testCase = Integer.parseInt(br.readLine());
-    	Object[][] arr = new Object[testCase][2];
-    	
-    	for(int i = 0; i < testCase; i++) {
-    		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
-    		
-    		arr[i][0] = Integer.parseInt(str.nextToken());
-    		arr[i][1] = str.nextToken();
-    	}
-    	
-    	Arrays.sort(arr, new Comparator<Object[]>() {
-    	    @Override
-    	    public int compare(Object[] o1, Object[] o2) {
-    	        return (Integer)o1[0] - (Integer)o2[0];
-    	    }
-    	});
-    	
-    	// 향상된 for문으로 출력
-        for (Object[] pair : arr) {
-            System.out.println(pair[0] + " " + pair[1]);
+    static class Pair {
+        int num;
+        String text;
+        Pair(int num, String text) {
+            this.num = num;
+            this.text = text;
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+        Pair[] arr = new Pair[testCase];
+
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int num = Integer.parseInt(st.nextToken());
+            String text = st.nextToken();
+            arr[i] = new Pair(num, text);
+        }
+
+        Arrays.sort(arr, (a, b) -> a.num - b.num);
+
+        StringBuilder sb = new StringBuilder();
+        for (Pair p : arr) {
+            sb.append(p.num).append(" ").append(p.text).append("\n");
+        }
+
+        System.out.print(sb);
     }
 }
